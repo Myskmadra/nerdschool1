6bf453d2c1e08f531031955f16856751868217ac
Martin Rippin <martin.rippin@web.de>
2019-10-01 19:00:42 +0200

    Feat: Introducing router

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const tvShowRouter = require('./tvShow/tvShowRouter');
 const app = express();
 const port = 8080;
 
 // Simple Hello World endpoint
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/tvshow', tvShowRouter);
 app.get('/', (request, response) => response.send('Hello World'));
 /*
 app.get('/', function (request, response) {
