ae3f5a4094cfd372bb3eeb80d7dc84923f894cab
Martin Rippin <martin.rippin@web.de>
2019-10-01 18:40:49 +0200

    Feat: Added verbosity with morgan

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 8080;
 
 // Simple Hello World endpoint
+app.use(morgan('dev'));
 app.get('/', (request, response) => response.send('Hello World'));
 /*
 app.get('/', function (request, response) {
